f82f6a9942a566cabe293bfd71c4521cc317106f
Jose Muñoz <tu-email@example.com>
2025-12-06 22:03:06 -0300

    feat: reemplazar proyectos web por proyectos Android en home

--- src/data/projetsAboutData.js
+++ src/data/projetsAboutData.js
@@ -1,5 +1,6 @@
-import urbanStyle from "../assets/img-projects/urbanStyle.png";
-import imgNote from "../assets/img-projects/note-app.png";
+import mackup1 from "../assets/img-projects/mackup1.png";
+import agendaPersonal from "../assets/img-projects/IMG-20251018-WA0021.jpg";
+import referidosApp from "../assets/img-projects/referidosapp.png";
 
 export const aboutData = {
   name: "Jose Muñoz Nuñez",
@@ -14,33 +15,27 @@ export const aboutData = {
 
 export const proyectos = [
   {
-    titulo: "E-commerce Urban Style",
-    descripcion: "Descripción breve del proyecto 1.",
-    imagen: urbanStyle.src,
-    tecnologias: ["React", "Tailwind", "Firebase"],
-    demo: "https://proyecto-final-rodriguez-sable.vercel.app/",
-    codigo: "https://github.com/joseMunozNunez10/ProyectoFinal-Rodriguez.git",
+    titulo: "App Gala Taiel",
+    descripcion: "Aplicación móvil Android con funciones CRUD, creación de tableros y listas, autenticación de usuarios y sincronización en tiempo real, desarrollada con Kotlin, MVVM, Jetpack y Firebase",
+    imagen: mackup1.src,
+    tecnologias: ["Kotlin", "Jetpack", "Firebase", "MVVM"],
+    demo: "",
+    codigo: "https://github.com/joseMunozNunez10/Gala8voTaiel",
   },
   {
-    titulo: "Tienda Artesana",
-    imagen:
-      "https://i.pinimg.com/736x/c9/70/bf/c970bfbc24489e39418b2c1b1f8f74ef.jpg",
-    tecnologias: ["React", "Node", "Express", "MongoDB"],
-    demo: "https://c21-05-ft-node-react-mirror-frontend-64qbwssi2-muyvisual.vercel.app/",
-    codigo: "https://github.com/No-Country-simulation/c21-05-ft-node-react",
+    titulo: "Agenda Personal",
+    descripcion: "AgendaPersonal es tu asistente ideal para organizar tu vida. Gestiona eventos con alarmas y notificaciones, y mantén tus finanzas bajo control. ¡Todo en una app moderna y fácil de usar!",
+    imagen: agendaPersonal.src,
+    tecnologias: ["Kotlin", "MVVM", "Hilt", "Jetpack Navigation"],
+    demo: "",
+    codigo: "https://github.com/joseMunozNunez10/AgendaPersonal",
   },
   {
-    titulo: "App organizacion",
-    imagen: imgNote.src,
-    tecnologias: ["React", "Tailwind", "Express", "MongoDB"],
-    demo: "https://front-react-todo.onrender.com",
-    codigo: "https://github.com/joseMunozNunez10/Front-react-todo",
-  },
-  {
-    titulo: "API REST",
-    imagen: "https://miro.medium.com/1*47S4iWVwTicFkFL4-Z4uAA.png",
-    tecnologias: ["Express", "Node.js", "MongoDB", "Mongoose"],
-    demo: "https://back-express-todo.onrender.com",
-    codigo: "https://github.com/joseMunozNunez10/Back-express-todo.git",
+    titulo: "Referidos App",
+    descripcion: "Aplicación móvil Android en desarrollo para gestionar y compartir referidos de productos y servicios, con autenticación de usuarios, almacenamiento local y sincronización en la nube",
+    imagen: referidosApp.src,
+    tecnologias: ["Kotlin", "Jetpack", "Firebase", "MVVM"],
+    demo: "",
+    codigo: "https://github.com/joseMunozNunez10/ReferidosApp",
   },
 ];
